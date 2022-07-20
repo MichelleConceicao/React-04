a7020a41f010902ba1fff7ec313d603035d673dd
MichelleConceição <liliandlentretenimento@gmail.com>
2022-07-20 10:29:31 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ export default class App extends Component{
   texto=() => {
     return <h1>Praticando Arrow Function</h1>
   }
-  triplo=(x) => {
-    return x*3
+  dobro=(x) => {
+    return x*2
   }
   render(){
     return(
       <>
       {this.texto()}
-      <h2>{this.triplo(6)}</h2>
+      <h2>{this.dobro(6)}</h2>
       </>
     )
   }
